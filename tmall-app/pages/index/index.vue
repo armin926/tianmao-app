7dea82a35ac599b14d20d0432ad9aa98e253edfc
armin <122699029@qq.com>
2020-12-03 21:36:00 +0800

    feat: tabBar

---
+++ tmall-app/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		首页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+</style>
